a42364d3208568a89d2acbe99754e0c9c6b484f6
shakerlu <77154621+walkingheartt1@users.noreply.github.com>
2021-11-03 17:25:21 +0800

    Update 1234..cpp

--- 1234..cpp
+++ 1234..cpp
@@ -2,23 +2,6 @@
 
 using namespace std;
 
-int fnon(int n)
-{
-    int x;
-    if (n>2)
-    {
-        x=n+1;  
-        return x;   
-    }
-    else
-    {
-        
-        return   0;
-    }
-     
-}
-
-
 int main()
 {
     int x;
